b143bdb84aa9e47ac34374140c51dd54815d4015
clz <fadinglight9291117@126.com>
2021-07-27 19:15:59 +0800

    go

---
+++ leetcode-go/114flatten-binary-tree-to-linked-list.go
@@ -0,0 +1,45 @@
+package main
+
+/**
+ *
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func flatten(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	res := preorder(root, make([]*TreeNode, 0, 10))
+	for i := 0; i < len(res)-1; i++ {
+		res[i].Right = res[i+1]
+		res[i].Left = nil
+	}
+}
+
+func preorder(root *TreeNode, res []*TreeNode) []*TreeNode {
+	if root == nil {
+		return res
+	}
+	res = append(res, root)
+	res = preorder(root.Left, res)
+	res = preorder(root.Right, res)
+	return res
+}
+
+func main() {
+	root := &TreeNode{}
+	root.Val = 1
+	left := &TreeNode{}
+	left.Val = 2
+	right := &TreeNode{}
+	right.Val = 3
+	root.Left = left
+	root.Right = right
+	flatten(root)
+	print("end\n")
+}
