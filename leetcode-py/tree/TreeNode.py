7348e5f93893a071e472ca2daad2a91a1fe02500
clz <fadinglight9291117@126.com>
2021-08-09 14:32:20 +0800

    add go

---
+++ leetcode-py/tree/TreeNode.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val, left, right):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+
+
+def deeporder(root: TreeNode) -> List[TreeNode]:
+    
+
+
+    
